13009586c8face13b9a0f5758c17f0d9573e85a4
hafeezrq <hafeezrq@yahoo.com>
2022-09-27 18:04:54 +1000

    Make use of connect from redux to domonstrate how props communicated and store updated.

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 export class SongsList extends Component {
   renderList() {
@@ -7,7 +8,11 @@ export class SongsList extends Component {
       return (
         <div className='item' key={song.title}>
           <div className='right floated content'>
-            <button className='ui button primary'>Select</button>
+            <button
+              className='ui button primary'
+              onClick={() => this.props.selectSong(song)}>
+              Select
+            </button>
           </div>
           <div className='content'>{song.title}</div>
         </div>
@@ -23,4 +28,4 @@ const mapStateToProps = state => {
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongsList);
+export default connect(mapStateToProps, { selectSong })(SongsList);
